26691562eb93a53c31ce0c76618082feea739ec6
Vincde <145802178+Vincde@users.noreply.github.com>
2024-04-05 16:05:10 +0200

    //

--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
-function newProject(name){
-    this.name = name;
-    this.todo = [];
-
-    const getName = () => {
-        return this.name;
-    }
-
-    const addTodo = (newElement) => {
-        this.todo.push(newElement);
-    }
-
-    return {getName,addTodo};
-}
-
-
-function start(){
-    let listOfProjects = [];
+function logic() {
+    /* this will be the explanation of this webpage
     
-    //button pressed
-    let name = prompt('name:');
-
-    let newProject = newProject(name);
-    listOfProjects.push(newProject);
-
-
-    
-
-
+    1) webpage starts
+    1*)webpage has another button for deleting project(and iife gets the value of the current clicked button)
+    when deleting button is pressed a function searches for the element and deletes the entire project and resets all elements at screen (and the button in dom obviously)
+    2)only present button is input and newProject button
+    3)if input is filled the button works and starts the creation of the project
+    4)the button creates the project
+    5)it creates a new button that resets the view of elements in the main part of webpage
+    6)only when clicked the newly created button prints all dom elements into the page
+    7)when another button is selected the dom elements gets deleted and new ones are printed
+    8)another iife gets the value of the currently clicked button and uses a button that takes a new todo and push it into the array of todos. The print function will print everything without you thinking about it
+    */
 }
