c1621ddba09c1d36ad7e80a50bda4860832e1fbc
Vincde <145802178+Vincde@users.noreply.github.com>
2024-04-27 21:42:05 +0200

    //

--- src/index.js
+++ src/index.js
@@ -22,3 +22,38 @@ function createNewProject(name){
     const newProject = new Project(name);
     return newProject;
 }
+
+
+
+
+
+/* ------------------------------------------------------------------------------
+                DOM EXECUTION
+                
+---------------------------------------------------------------------------------*/
+
+function execDOM(){
+    const newProject = document.querySelector('.buttons button:first-of-type');
+
+    newProject.addEventListener('click', ()=>{
+        drawForm();
+        //1)function that asks name
+        //2)create the new project
+        //3)put project into array of projects
+        //4)create button with the name of the new project
+        //5)Put new button into webpage
+    });
+}
+
+
+function drawForm(){
+    const newContainer = document.createElement('div');
+    const newLabel = document.createElement('label');
+    const newInput = document.createElement('input');
+
+    newInput.setAttribute('id','NewProjectName');
+    newLabel.setAttribute('for','NewProjectName');
+
+    
+
+}
